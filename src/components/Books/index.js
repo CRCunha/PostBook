c54612669b27eaf834b08fec260825790f8dc900
CRCunha <contato@cristiancunha.me>
2019-10-23 07:13:23 -0300

    Seacrh Bar - Books

---
+++ src/components/Books/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './books.css'
+import SearchImg from './IMG/search.png'
+
+const Books = () => (
+    <div className="books">
+        <div className="container">
+            <div className="search">
+                <form>
+                    <input type='text' name='busca' autoComplete='off' />
+                    <div className="btnSearch">
+                        <input type="submit" name="enviar" value="" />
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+);
+
+export default Books;
