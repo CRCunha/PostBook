e41f17aa05460c9c859f33e681472a813310cca3
CRCunha <contato@cristiancunha.me>
2019-11-10 09:39:15 -0200

    excluir livro

--- Api/Controllers/livros.js
+++ Api/Controllers/livros.js
@@ -33,50 +33,18 @@ router.get('/', (request, response) => {
     }); 
 });
 
-// DELETA AS CATEGORIAS INFORMADAS
+// DELETE LIVRO
+router.delete('/:id', (request, response) => {
+    let id = request.params.id;
 
-// router.delete('/:id', (request, response) => {
-
-//     // Se Deletado estiver em FALSE, a categoria não foi excluida, se estiver em TRUE, a categoria foi 'excluida' 
-//     bancoDeDados.conexao.query(`select * from Categoria where Deletado = false and Id = ${request.params.id}`, (erro, resultado) => {
-//         if (resultado.length > 0)
-//             response.status(200).send('O id foi encontrado e Deletado com sucesso!');
-//         else
-//             response.status(404).json({ resultado: 'O id não foi encontrado!' });
-//         if (!erro)
-//             bancoDeDados.conexao.query(`update Categoria set Deletado = true where Id = ${request.params.id}`, (deletado, result) => {
-//                 if (!deletado)
-//                     response.status(200).send();
-
-//                 else
-//                     response.status(400).json({ deletado: deletado });
-
-
-//             });
-
-//     });
-// });
-
-// ATUALIZA A CATEGORIA INFORMADA
-
-// router.put('/:id', (request, response) => {
-//     bancoDeDados.conexao.query(`select * from Categoria where Deletado = false and Id = ${request.params.id}`,
-//         (erro, resultado) => {
-//             if (resultado.length === 0)
-//                 response.status(404).send('Id informado não foi encontrado');
-//             if (!erro)
-//                 bancoDeDados.conexao.query(`update Categoria set Titulo = '${request.body.titulo}', 
-//                Text = '${request.body.text}' where Id = ${request.params.id}`,
-//                     (erro, resultado) => {
-//                         if (erro)
-//                             response.status(400).json({ erro: erro });
-//                         else
-//                             response.status(200).send('A categoria informada foi atualizada com sucesso!');
-//                     });
-//             else
-//                 response.status(400).json({ Erro: erro });
-//         });
-
-// });
+    bancoDeDados.conexao.query(`delete from Livros where '${id}' = id`, (erro, resultado) => {
+        if (!erro) {
+            response.status(200).json(resultado);
+        }
+        else {
+            response.status(400).json({ erro: erro });
+        }
+    });
+});
 
 module.exports = (api) => api.use('/api/livros', router);
